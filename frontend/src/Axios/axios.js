0ef1e0b32dcdc4a303fcb0a1cb2a3285ea1cd899
Shubham Yadav <shubhamyadav30781@gmail.com>
2025-09-17 21:10:38 +0530

    change axios base url

--- frontend/src/Axios/axios.js
+++ frontend/src/Axios/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios"
 const instance = axios.create({
-    baseURL:"http://localhost:8000/api"
-})
+  baseURL: "https://taskflo-backend.onrender.com/api",
+});
 export default instance
